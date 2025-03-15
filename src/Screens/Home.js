468b018ae91c4180a536ba000fbffd2b3009c6f8
JdmKash <jdmkash@gmail.com>
2025-03-15 03:47:31 -0400

    Add files via upload

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,16 +1,11 @@
+// Home.js
 import React from "react";
 import Liders from "../Components/Liders"
 import MiningButton from "../Components/MiningButton";
 import UserRank from "../Components/userRank";
-import { useSelector } from "react-redux";
-import { selectUser } from "../features/userSlice";
-import LoadingModul from "../Components/LoadingModul";
-import backgroundImage from "../Assets/auction-2.png";
 
 function Home() {
-  const user = useSelector(selectUser);
-  
-  // Super simple Home component that doesn't depend on calculate state
+  // Super simple Home component that doesn't depend on any state
   return (
     <div className="flex flex-col h-screen relative">
       <div className="flex items-center justify-venter mt-16">
