5cdeec71e9ec25ae7306a8be7a603f74a2e66a9f
JdmKash <jdmkash@gmail.com>
2025-03-18 19:43:21 -0400

    Update calculateSlice.js

--- src/features/calculateSlice.js
+++ src/features/calculateSlice.js
@@ -1,7 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: null,
+  value: {
+    miningStartedTime: null,
+    mined: 0,
+    progress: 0,
+    remainingTime: { hours: 0, minutes: 0, seconds: 0 },
+    canClaim: false,
+    canUpgrade: false
+  },
 };
 
 export const calculateSlice = createSlice({
@@ -11,11 +18,42 @@ export const calculateSlice = createSlice({
     setCalculated: (state, action) => {
       state.value = action.payload;
     },
+    startMining: (state, action) => {
+      const { miningStartedTime } = action.payload;
+      state.value = {
+        ...state.value,
+        miningStartedTime,
+        mined: 0,
+        progress: 0,
+        remainingTime: { hours: 6, minutes: 0, seconds: 0 },
+        canClaim: false
+      };
+    },
+    stopMining: (state) => {
+      state.value = {
+        ...state.value,
+        miningStartedTime: null,
+        progress: 0,
+        remainingTime: { hours: 0, minutes: 0, seconds: 0 },
+        canClaim: false
+      };
+    },
+    resetMining: (state) => {
+      state.value = {
+        ...state.value,
+        miningStartedTime: null,
+        mined: 0,
+        progress: 0,
+        remainingTime: { hours: 0, minutes: 0, seconds: 0 },
+        canClaim: false
+      };
+    }
   },
 });
 
-export const { setCalculated } = calculateSlice.actions;
+export const { setCalculated, startMining, stopMining, resetMining } = calculateSlice.actions;
 
 export const selectCalculated = (state) => state.calculate.value;
+export const selectCalculate = (state) => state.calculate.value;
 
 export default calculateSlice.reducer;
